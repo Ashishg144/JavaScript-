926a86c477b750466afb80f864d89e1b895a04a3
Ashish gupta <guptaashish09k@gmail.com>
2026-09-08 04:57:14 +0000

    commit config done

---
+++ 01_basics/01_var.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let accountEmail = "ashish@gmail.com";
+var accountPassword = "12098";
+accountcity = "mumbai";
+let accountState;
+
+
+// 2nd account ( change):-
+
+accountEmail = "ashi@gmail.com";
+accountPassword = "12154";
+accountcity = "Delhi";
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
