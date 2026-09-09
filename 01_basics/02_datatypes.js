b6f679695b20af7967a984c7bb7f491d6f47e916
Ashish gupta <guptaashish09k@gmail.com>
2026-09-09 05:11:29 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+let name = "Ashish";     //string
+let age = 20;            //Number
+let isStudent = true;    //Boolean
+let salary = 2013485943395;   //BigInt
+let score;                    //undefine
+let data = null;              // null
+let uniqueId = Symbol("id");  //symbol
+
+console.log(name);
+console.log(typeof(name));
+
+console.log(age);
+console.log(typeof(age));
+
+console.log(isStudent);
+console.log(typeof(isStudent));
+
+console.log(salary);
+console.log(typeof(salary));
+
+console.log(score);
+console.log(typeof(score));
+
+console.log(data);
+console.log(typeof(data));
+
+console.log(uniqueId);
+console.log(typeof(uniqueId));
+
+
+
---
+++ 01_basics/03_datatypesConversion.js
@@ -0,0 +1,16 @@
+//1)
+let age = 22;
+//console.log(typeof(age));
+
+let agestring = String(age);
+//console.log(agestring);
+//console.log(typeof(agestring));
+
+//2)
+let name = "Ashish";
+console.log(typeof(name));
+
+let number_name = Number(name);
+console.log(number_name);
+
+
